fe610ea702bb9d89bd8574c93379f40c6484c0db
Hernando Rey <hrking31@gmail.com>
2025-08-11 10:31:50 -0500

    se mejora efecto en cinta informativa permitiendo un desplazamiento infinito del mensaje

--- src/Components/TickerMessage/TickerMessage.jsx
+++ src/Components/TickerMessage/TickerMessage.jsx
@@ -1,11 +1,15 @@
-import React from "react";
 
 export default function TickerMessage({ message }) {
   return (
     <div className="w-full fixed bottom-0 z-50 bg-black py-2 overflow-hidden">
-      <div className="flex whitespace-nowrap animate-marquee">
-        <span className="text-yellow-300 font-bold text-2xl px-4">
-          {message}
+      <div className="flex animate-marquee">
+        <span className="flex whitespace-nowrap">
+          <span className="text-yellow-300 font-bold text-2xl px-4">
+            {message}
+          </span>
+          <span className="text-yellow-300 font-bold text-2xl px-4">
+            {message}
+          </span>
         </span>
       </div>
     </div>
